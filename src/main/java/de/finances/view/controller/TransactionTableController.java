0479a5990a81bd3be2415b8279104feba6ef0f42
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2018-10-16 19:26:55 +0200

    Change items according to the selected month

--- src/main/java/de/finances/view/controller/TransactionTableController.java
+++ src/main/java/de/finances/view/controller/TransactionTableController.java
@@ -10,6 +10,7 @@ import de.finances.application.model.Category;
 import de.finances.application.model.Transaction;
 import de.finances.application.service.TransactionService;
 import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -51,17 +52,24 @@ public class TransactionTableController implements Initializable {
 		this.month.setValue(LocalDate.now().getMonth());
 		this.year.setValue(LocalDate.now().getYear());
 
+		this.month.valueProperty().addListener((ChangeListener<Month>) (observable, oldValue,
+				newValue) -> TransactionTableController.this.loadTransactions());
+
 		this.month.setItems(
 				FXCollections.observableArrayList(this.transactionService.findAllUsesdMonth(this.year.getValue())));
 		this.year.setItems(FXCollections.observableArrayList(this.transactionService.findAllUsesdYears()));
-		this.transactionTable.setItems(FXCollections.observableArrayList(
-				this.transactionService.getTransactionsByMonthAndYear(this.year.getValue(), this.month.getValue())));
+		this.loadTransactions();
 
 		this.columnDay.setCellValueFactory(p -> new ReadOnlyObjectWrapper<>(p.getValue().getCreated().getDayOfMonth()));
 		this.columnLabel.setCellValueFactory(new PropertyValueFactory<Transaction, String>("label"));
 		this.columnCategory.setCellValueFactory(new PropertyValueFactory<Transaction, Category>("category"));
 		this.columnValue.setCellValueFactory(new PropertyValueFactory<Transaction, BigDecimal>("value"));
 
+	}
+
+	private void loadTransactions() {
+		this.transactionTable.getItems().setAll(FXCollections.observableArrayList(
+				this.transactionService.getTransactionsByMonthAndYear(this.year.getValue(), this.month.getValue())));
 	}
 
 }
